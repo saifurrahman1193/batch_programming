fce5c35205beee86f993d87b41fc97728a77da57
saifur rahman <saifuroracle@gmail.com>
2022-07-03 21:26:15 +0600

    updated

--- git/saifur_master_desktop/git.bat
+++ git/saifur_master_desktop/git.bat
@@ -1,15 +1,46 @@
 cd /
 E:
-cd E:\Projects\3.ssl_wireless\8.games_admin_api
-git pull 
+cd E:\Projects\3.ssl_wireless\1.ghoori_admin
+git pull
+cd E:\Projects\3.ssl_wireless\1.ghoori_api
+git pull
+cd E:\Projects\3.ssl_wireless\2.maasranga_television_admin
+git pull
+cd E:\Projects\3.ssl_wireless\2.maasranga_television_api
+git pull
+cd E:\Projects\3.ssl_wireless\3.radio_din_rat_admin
+git pull
+cd E:\Projects\3.ssl_wireless\3.radio_din_rat_api
+git pull
+cd E:\Projects\3.ssl_wireless\4.vr_report_admin
+git pull
+cd E:\Projects\3.ssl_wireless\4.vr_report_admin_api
+git pull
+cd E:\Projects\3.ssl_wireless\5.rongila_cms
+git pull
+cd E:\Projects\3.ssl_wireless\5.rongila_cms_api
+git pull
+cd E:\Projects\3.ssl_wireless\5.rongila_site
+git pull
+cd E:\Projects\3.ssl_wireless\5.rongila_web_api
+git pull
+
+cd E:\Projects\3.ssl_wireless\6.marks2021
+git pull
+cd E:\Projects\3.ssl_wireless\7.ssg2021
+git pull
 cd E:\Projects\3.ssl_wireless\8.games_admin
+git pull
+cd E:\Projects\3.ssl_wireless\8.games_admin_api
 git pull
 cd E:\Projects\3.ssl_wireless\9.cmp_api
 git pull
 cd E:\Projects\3.ssl_wireless\9.cmp_frontend
 git pull
-cd E:\Projects\3.ssl_wireless\9.cmp_api
+
+E:\Projects\3.ssl_wireless\10.cokewc22concert
 git pull
-cd E:\Projects\3.ssl_wireless\6.marks2021
+E:\Projects\3.ssl_wireless\10.cokewc22microsite
 git pull
-pause
+E:\Projects\3.ssl_wireless\11.push_pull_app_api
+git pull
