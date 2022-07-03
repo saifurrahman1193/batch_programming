2eb40eb09d72b9b32f6dd9c0812b7ec02c890519
saifur rahman <saifuroracle@gmail.com>
2022-07-03 21:16:02 +0600

    updated

---
+++ git/saifur_master_desktop/git.bat
@@ -0,0 +1,15 @@
+cd /
+E:
+cd E:\Projects\3.ssl_wireless\8.games_admin_api
+git pull 
+cd E:\Projects\3.ssl_wireless\8.games_admin
+git pull
+cd E:\Projects\3.ssl_wireless\9.cmp_api
+git pull
+cd E:\Projects\3.ssl_wireless\9.cmp_frontend
+git pull
+cd E:\Projects\3.ssl_wireless\9.cmp_api
+git pull
+cd E:\Projects\3.ssl_wireless\6.marks2021
+git pull
+pause
