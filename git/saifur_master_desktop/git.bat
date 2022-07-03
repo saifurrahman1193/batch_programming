91f5c6cfae7113259e9efa208443cb911dc56d67
saifur rahman <saifuroracle@gmail.com>
2022-07-03 21:51:46 +0600

    updated

--- git/saifur_master_desktop/git.bat
+++ git/saifur_master_desktop/git.bat
@@ -4,64 +4,148 @@ E:
 echo ...................SSL Wireless all projects............................
 cd E:\Projects\3.ssl_wireless\1.ghoori_admin
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\1.ghoori_api
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\2.maasranga_television_admin
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\2.maasranga_television_api
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\3.radio_din_rat_admin
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\3.radio_din_rat_api
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\4.vr_report_admin
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\4.vr_report_admin_api
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\5.rongila_cms
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\5.rongila_cms_api
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\5.rongila_site
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\5.rongila_web_api
 git pull
+git add .
+git commit -m "Auto push"
+git push
 
 cd E:\Projects\3.ssl_wireless\6.marks2021
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\7.ssg2021
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\8.games_admin
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\8.games_admin_api
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\9.cmp_api
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\9.cmp_frontend
 git pull
+git add .
+git commit -m "Auto push"
+git push
 
 cd E:\Projects\3.ssl_wireless\10.cokewc22concert
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\10.cokewc22microsite
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\3.ssl_wireless\11.push_pull_app_api
 git pull
+git add .
+git commit -m "Auto push"
+git push
 
 
 echo ...................Personal all projects............................
 cd E:\Projects\0.personal\socketio
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\0.personal\indexeddb
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\0.personal\js_php_python
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\0.personal\docker_cmp
 git pull
+git add .
+git commit -m "Auto push"
+git push
 
 echo ...................Important projects............................
 cd E:\Projects\batch_programming
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\db_backups
 git pull
+git add .
+git commit -m "Auto push"
+git push
 cd E:\Projects\templates
 git pull
+git add .
+git commit -m "Auto push"
+git push
