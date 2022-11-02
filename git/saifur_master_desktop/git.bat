a9e0a316b512fe5531d0e27f735ce5494f8a1b7a
saifur rahman <saifur.rahman1193@gmail.com>
2022-11-02 21:41:22 +0600

    updated

--- git/saifur_master_desktop/git.bat
+++ git/saifur_master_desktop/git.bat
@@ -12,6 +12,7 @@ git pull
 git add .
 git commit -m "Auto push"
 git push
+
 cd F:\Projects\3.ssl_wireless\2.maasranga_television_admin
 git pull
 git add .
@@ -22,6 +23,7 @@ git pull
 git add .
 git commit -m "Auto push"
 git push
+
 cd F:\Projects\3.ssl_wireless\3.radio_din_rat_admin
 git pull
 git add .
@@ -32,6 +34,7 @@ git pull
 git add .
 git commit -m "Auto push"
 git push
+
 cd F:\Projects\3.ssl_wireless\4.vr_report_admin
 git pull
 git add .
@@ -42,6 +45,7 @@ git pull
 git add .
 git commit -m "Auto push"
 git push
+
 cd F:\Projects\3.ssl_wireless\5.rongila_cms
 git pull
 git add .
@@ -63,16 +67,18 @@ git add .
 git commit -m "Auto push"
 git push
 
-cd F:\Projects\3.ssl_wireless\6.marks2021
-git pull
-git add .
-git commit -m "Auto push"
-git push
-cd F:\Projects\3.ssl_wireless\7.ssg2021
-git pull
-git add .
-git commit -m "Auto push"
-git push
+@REM cd F:\Projects\3.ssl_wireless\6.marks2021
+@REM git pull
+@REM git add .
+@REM git commit -m "Auto push"
+@REM git push
+
+@REM cd F:\Projects\3.ssl_wireless\7.ssg2021
+@REM git pull
+@REM git add .
+@REM git commit -m "Auto push"
+@REM git push
+
 cd F:\Projects\3.ssl_wireless\8.games_admin
 git pull
 git add .
@@ -83,6 +89,7 @@ git pull
 git add .
 git commit -m "Auto push"
 git push
+
 cd F:\Projects\3.ssl_wireless\9.cmp_api
 git pull
 git add .
@@ -94,22 +101,36 @@ git add .
 git commit -m "Auto push"
 git push
 
-cd F:\Projects\3.ssl_wireless\10.cokewc22concert
-git pull
-git add .
-git commit -m "Auto push"
-git push
-cd F:\Projects\3.ssl_wireless\10.cokewc22microsite
-git pull
-git add .
-git commit -m "Auto push"
-git push
+@REM cd F:\Projects\3.ssl_wireless\10.cokewc22concert
+@REM git pull
+@REM git add .
+@REM git commit -m "Auto push"
+@REM git push
+@REM cd F:\Projects\3.ssl_wireless\10.cokewc22microsite
+@REM git pull
+@REM git add .
+@REM git commit -m "Auto push"
+@REM git push
+
 cd F:\Projects\3.ssl_wireless\11.push_pull_app_api
 git pull
 git add .
 git commit -m "Auto push"
 git push
+
 cd F:\Projects\3.ssl_wireless\12.otl_report
+git pull
+git add .
+git commit -m "Auto push"
+git push
+
+cd F:\Projects\3.ssl_wireless\13.bl_sdp
+git pull
+git add .
+git commit -m "Auto push"
+git push
+
+cd F:\Projects\3.ssl_wireless\14.otl_erp
 git pull
 git add .
 git commit -m "Auto push"
