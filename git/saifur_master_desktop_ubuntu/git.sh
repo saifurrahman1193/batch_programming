3072030260116689516e047fe5e69d43e8c635d1
saifur rahman <saifur.rahman1193@gmail.com>
2022-11-20 21:18:39 +0600

    updated

---
+++ git/saifur_master_desktop_ubuntu/git.sh
@@ -0,0 +1,182 @@
+echo "...................SSL Wireless all projects............................"
+# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/1.ghoori_admin"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/1.ghoori_api"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/2.maasranga_television_admin"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/2.maasranga_television_api"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/3.radio_din_rat_admin"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/3.radio_din_rat_api"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/4.vr_report_admin"
+git pull
+git add .
+git commit -m "Auto push"
+git push
+cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/4.vr_report_admin_api"
+git pull
+git add .
+git commit -m "Auto push"
+git push
+
+# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/5.rongila_cms"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/5.rongila_cms_api"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/5.rongila_site"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/5.rongila_web_api"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/6.marks2021"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/7.ssg2021"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/8.games_admin"
+git pull
+git add .
+git commit -m "Auto push"
+git push
+cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/8.games_admin_api"
+git pull
+git add .
+git commit -m "Auto push"
+git push
+
+cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/9.cmp_api"
+git pull
+git add .
+git commit -m "Auto push"
+git push
+cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/9.cmp_frontend"
+git pull
+git add .
+git commit -m "Auto push"
+git push
+
+# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/10.cokewc22concert"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/10.cokewc22microsite"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/11.push_pull_app_api"
+git pull
+git add .
+git commit -m "Auto push"
+git push
+
+cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/12.otl_report"
+git pull
+git add .
+git commit -m "Auto push"
+git push
+
+cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/13.bl_sdp"
+git pull
+git add .
+git commit -m "Auto push"
+git push
+
+cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/14.otl_erp"
+git pull
+git add .
+git commit -m "Auto push"
+git push
+
+
+echo ...................Personal all projects............................
+cd /media/saifur/1800 GB Don't Format !!!/Projects/0.personal/socketio
+git pull
+git add .
+git commit -m "Auto push"
+git push
+cd /media/saifur/1800 GB Don't Format !!!/Projects/0.personal/indexeddb
+git pull
+git add .
+git commit -m "Auto push"
+git push
+cd /media/saifur/1800 GB Don't Format !!!/Projects/0.personal/js_php_python
+git pull
+git add .
+git commit -m "Auto push"
+git push
+cd /media/saifur/1800 GB Don't Format !!!/Projects/0.personal/docker_cmp
+git pull
+git add .
+git commit -m "Auto push"
+git push
+
+cd /media/saifur/1800 GB Don't Format !!!/Projects/0.personal/react-learning
+git pull
+git add .
+git commit -m "Auto push"
+git push
+
+echo ...................Important projects............................
+cd "/media/saifur/1800 GB Don't Format !!!/Projects/batch_programming"
+git pull
+git add .
+git commit -m "Auto push"
+git push
+
+cd "/media/saifur/1800 GB Don't Format !!!/Projects/db_backups"
+git pull
+git add .
+git commit -m "Auto push"
+git push
+
+cd "/media/saifur/1800 GB Don't Format !!!/Projects/templates"
+git pull
+git add .
+git commit -m "Auto push"
+git push
---
+++ git/saifur_master_desktop_ubuntu/test.sh
@@ -0,0 +1,8 @@
+echo "Hello, world!"
+pwd
+cd "/media/saifur/1800 GB Don't Format !!!/"
+pwd
+echo "...................SSL Wireless all projects............................"
+
+cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless"
+
