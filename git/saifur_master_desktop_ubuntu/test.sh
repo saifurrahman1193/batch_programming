cfda0143efd2a32850074a8b9d2b843b50e82651
saifur rahman <saifur.rahman1193@gmail.com>
2022-11-20 23:05:06 +0600

    updated

--- git/saifur_master_desktop_ubuntu/test.sh
+++
@@ -1,8 +0,0 @@
-echo "Hello, world!"
-pwd
-cd "/media/saifur/1800 GB Don't Format !!!/"
-pwd
-echo "...................SSL Wireless all projects............................"
-
-cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless"
-
