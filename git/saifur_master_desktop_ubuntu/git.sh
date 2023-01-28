43d15671a23aff66a5368af3079010c4f27b2c1d
saifur rahman <saifur.rahman1193@gmail.com>
2023-01-28 15:27:32 +0600

    Auto push

--- git/saifur_master_desktop_ubuntu/git.sh
+++ git/saifur_master_desktop_ubuntu/git.sh
@@ -1,182 +1,188 @@
 echo "...................SSL Wireless all projects............................"
-# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/1.ghoori_admin"
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/1.ghoori_admin"
 # git pull
 # git add .
 # git commit -m "Auto push"
 # git push
-# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/1.ghoori_api"
-# git pull
-# git add .
-# git commit -m "Auto push"
-# git push
-
-# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/2.maasranga_television_admin"
-# git pull
-# git add .
-# git commit -m "Auto push"
-# git push
-# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/2.maasranga_television_api"
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/1.ghoori_api"
 # git pull
 # git add .
 # git commit -m "Auto push"
 # git push
 
-# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/3.radio_din_rat_admin"
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/2.maasranga_television_admin"
 # git pull
 # git add .
 # git commit -m "Auto push"
 # git push
-# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/3.radio_din_rat_api"
-# git pull
-# git add .
-# git commit -m "Auto push"
-# git push
-
-cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/4.vr_report_admin"
-git pull
-git add .
-git commit -m "Auto push"
-git push
-cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/4.vr_report_admin_api"
-git pull
-git add .
-git commit -m "Auto push"
-git push
-
-# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/5.rongila_cms"
-# git pull
-# git add .
-# git commit -m "Auto push"
-# git push
-# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/5.rongila_cms_api"
-# git pull
-# git add .
-# git commit -m "Auto push"
-# git push
-# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/5.rongila_site"
-# git pull
-# git add .
-# git commit -m "Auto push"
-# git push
-# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/5.rongila_web_api"
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/2.maasranga_television_api"
 # git pull
 # git add .
 # git commit -m "Auto push"
 # git push
 
-# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/6.marks2021"
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/3.radio_din_rat_admin"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/3.radio_din_rat_api"
 # git pull
 # git add .
 # git commit -m "Auto push"
 # git push
 
-# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/7.ssg2021"
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/4.vr_report_admin"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/4.vr_report_admin_api"
 # git pull
 # git add .
 # git commit -m "Auto push"
 # git push
 
-cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/8.games_admin"
-git pull
-git add .
-git commit -m "Auto push"
-git push
-cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/8.games_admin_api"
-git pull
-git add .
-git commit -m "Auto push"
-git push
-
-cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/9.cmp_api"
-git pull
-git add .
-git commit -m "Auto push"
-git push
-cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/9.cmp_frontend"
-git pull
-git add .
-git commit -m "Auto push"
-git push
-
-# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/10.cokewc22concert"
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/5.rongila_cms"
 # git pull
 # git add .
 # git commit -m "Auto push"
 # git push
-# cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/10.cokewc22microsite"
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/5.rongila_cms_api"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/5.rongila_site"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/5.rongila_web_api"
 # git pull
 # git add .
 # git commit -m "Auto push"
 # git push
 
-cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/11.push_pull_app_api"
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/6.marks2021"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/7.ssg2021"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/8.games_admin"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/8.games_admin_api"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/9.cmp_api"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/9.cmp_frontend"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+# # cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/10.cokewc22concert"
+# # git pull
+# # git add .
+# # git commit -m "Auto push"
+# # git push
+# # cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/10.cokewc22microsite"
+# # git pull
+# # git add .
+# # git commit -m "Auto push"
+# # git push
+
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/11.push_pull_app_api"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/12.otl_report"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/13.bl_sdp"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/3.ssl_wireless/14.otl_erp"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+
+# echo ...................Personal all projects............................
+# cd /mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/0.personal/socketio
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+# cd /mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/0.personal/indexeddb
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+# cd /mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/0.personal/js_php_python
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+# cd /mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/0.personal/docker_cmp
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+# cd /mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/0.personal/react-learning
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
+
+# echo ...................Important projects............................
+cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/batch_programming"
 git pull
 git add .
 git commit -m "Auto push"
 git push
 
-cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/12.otl_report"
+cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/db_backups"
 git pull
 git add .
 git commit -m "Auto push"
 git push
 
-cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/13.bl_sdp"
+cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/postman_backup"
 git pull
 git add .
 git commit -m "Auto push"
 git push
 
-cd "/media/saifur/1800 GB Don't Format !!!/Projects/3.ssl_wireless/14.otl_erp"
-git pull
-git add .
-git commit -m "Auto push"
-git push
-
-
-echo ...................Personal all projects............................
-cd /media/saifur/1800 GB Don't Format !!!/Projects/0.personal/socketio
-git pull
-git add .
-git commit -m "Auto push"
-git push
-cd /media/saifur/1800 GB Don't Format !!!/Projects/0.personal/indexeddb
-git pull
-git add .
-git commit -m "Auto push"
-git push
-cd /media/saifur/1800 GB Don't Format !!!/Projects/0.personal/js_php_python
-git pull
-git add .
-git commit -m "Auto push"
-git push
-cd /media/saifur/1800 GB Don't Format !!!/Projects/0.personal/docker_cmp
-git pull
-git add .
-git commit -m "Auto push"
-git push
-
-cd /media/saifur/1800 GB Don't Format !!!/Projects/0.personal/react-learning
-git pull
-git add .
-git commit -m "Auto push"
-git push
-
-echo ...................Important projects............................
-cd "/media/saifur/1800 GB Don't Format !!!/Projects/batch_programming"
-git pull
-git add .
-git commit -m "Auto push"
-git push
-
-cd "/media/saifur/1800 GB Don't Format !!!/Projects/db_backups"
-git pull
-git add .
-git commit -m "Auto push"
-git push
-
-cd "/media/saifur/1800 GB Don't Format !!!/Projects/templates"
-git pull
-git add .
-git commit -m "Auto push"
-git push
+# cd "/mnt/7e3701e0-10b9-4e10-8884-5b27c662033b/Projects/templates"
+# git pull
+# git add .
+# git commit -m "Auto push"
+# git push
